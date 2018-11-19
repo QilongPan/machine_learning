07e1ab1d586adc081ac4c7db8f97606ca2f8079e
QilongPan <3102377627@qq.com>
2018-11-19 20:36:45 +0800

    crea

--- reinforcement_learning/markov_decision_process/agent.py
+++ reinforcement_learning/markov_decision_process/agent.py
@@ -2,8 +2,14 @@
 # @Author: Qilong Pan
 # @Date:   2018-11-19 13:22:27
 # @Last Modified by:   Qilong Pan
-# @Last Modified time: 2018-11-19 13:25:54
+# @Last Modified time: 2018-11-19 20:34:43
 
 class Agent(object):
     def __init__(self):
         self.actions = ['up','down','left','right']
+
+
+class Env(object):
+    def __init__(self):
+        self.states = [1,2,3,4,5,6,7,8]
+        
