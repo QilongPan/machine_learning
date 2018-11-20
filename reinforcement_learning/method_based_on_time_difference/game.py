ba04a715d21dfdd189339bb02a9479fb68d56208
QilongPan <3102377627@qq.com>
2018-11-20 23:03:56 +0800

    finish sarsa and q_learning

--- reinforcement_learning/method_based_on_time_difference/game.py
+++ reinforcement_learning/method_based_on_time_difference/game.py
@@ -4,3 +4,81 @@
 # @Last Modified by:   Qilong Pan
 # @Last Modified time: 2018-11-20 18:47:08
 
+'''
+在更新St的动作值函数需要St+1的动作值函数，Sarsa用的是e-greedy方法，和选择St下的动作一样；
+而Q-learning是用的greedy方法和选择St下动作不一样，因此称为off-policy
+'''
+'''
+走迷宫例子
+0  1  2  3 
+4  5  6  7
+8  9  10 11
+12 13 14 15
+
+0和15为迷宫出口，机器人的起始位置是随机的
+'''
+import random
+
+class Game(object):
+
+    def __init__(self):
+        self.states = range(16)
+        self.actions = ['up','down','left','right']
+        self.gamma = 0.8
+
+    def do_action(self,state,action):
+        #到达终点，reward 为0,其余状态为-1
+        if state == 0 or state == 15:
+            return True,state,0
+        if action == 'up':
+            next_state = state - 4
+        elif action == 'down':
+            next_state = state + 4
+        elif action == 'left':
+            next_state = state - 1
+        elif action == 'right':
+            next_state = state + 1
+        if next_state < 0 or next_state > 15:
+            next_state = state
+        if next_state == 0 or next_state == 15:
+            return next_state
+            
+        if state == 3 and next_state == 4:
+            return state
+        elif state == 4 and next_state == 3:
+            return state
+        elif state == 7 and next_state == 8:
+            return state
+        elif state == 8 and next_state == 7:
+            return state
+        elif state == 11 and next_state == 12:
+            return state
+        elif state == 12 and next_state == 11:
+            return state
+
+        return next_state
+
+    def is_end(self,state):
+        if state == 0 or state == 15:
+            return True
+        else:
+            return False
+
+    '''
+    出迷宫奖励为0，没出动作为-1
+    '''
+    def get_reward(self,state,action):
+        if state == 1:
+            if action == 'left':
+                return 0
+        elif state == 4:
+            if action == 'up':
+                return 0
+        elif state == 11:
+            if action == 'down':
+                return 0
+        elif state == 14:
+            if action == 'right':
+                return 0
+        return -1
+
---
+++ reinforcement_learning/method_based_on_time_difference/q_learning.py
@@ -0,0 +1,79 @@
+
+import random
+from game import Game
+'''
+在更新St的动作值函数需要St+1的动作值函数，Sarsa用的是e-greedy方法，和选择St下的动作一样；
+而Q-learning是用的greedy方法和选择St下动作不一样，因此称为off-policy
+'''
+class QLearning(object):
+
+    def __init__(self):
+        self.game = Game()
+        self.iterator_number = 1000
+        self.state_action_values = []
+        for i in range(len(self.game.states)):
+            state_action_value = []
+            for j in range(len(self.game.actions)):
+                state_action_value.append(0)
+            self.state_action_values.append(state_action_value)
+        self.alpha = 0.2
+
+    #利用ε-greedy策略
+    def epsilon_greedy(self,state,epsilon):
+        best_action_index = 0
+        best_value = self.state_action_values[state][0]
+        for i in range(len(self.game.actions)):
+            if best_value < self.state_action_values[state][i]:
+                best_value = self.state_action_values[state][i]
+                best_action_index = i
+
+        probability = [0.0 for i in range(len(self.game.actions))]
+        probability[best_action_index] += 1 - epsilon
+        for i in range(len(self.game.actions)):
+            probability[i] += epsilon / len(self.game.actions)
+
+        random_number = random.random()
+        sum_probability = 0.0
+        for i in range(len(self.game.actions)):
+            sum_probability += probability[i]
+            if sum_probability >= random_number:
+                return i
+        return len(self.game.actions) - 1
+
+
+    def greedy(self,state):
+        best_action_index = 0
+        best_value = self.state_action_values[state][best_action_index]
+        for i in range(len(self.game.actions)):
+            if best_value < self.state_action_values[state][i]:
+                best_value = self.state_action_values[state][i]
+                best_action_index = i
+        return best_action_index
+    
+    def q_learning(self,epsilon):
+        for i in range(self.iterator_number):
+            state = self.game.states[random.randint(1,len(self.game.states) - 2)]
+            action_index = random.randint(0,len(self.game.actions) - 1)
+            move_number = 0
+            while not self.game.is_end(state) and move_number < 50:
+                next_state = self.game.do_action(state,self.game.actions[action_index])
+                if next_state == 0 or next_state == 15:
+                    reward = 1
+                else:
+                    reward = 0
+                #寻找St+1的最大动作值采用的是greedy策略
+                best_action_index = self.greedy(next_state)
+                self.state_action_values[state][action_index] += self.alpha * (reward + self.game.gamma * self.state_action_values[next_state][best_action_index] - self.state_action_values[state][action_index])
+                state = next_state
+                #寻找St的下一动作采用的是ε-greedy策略
+                #寻找St+1的最大动作值和寻找St的下一动作采用的策略不同，所以为异策略
+                action_index = self.epsilon_greedy(next_state,epsilon)
+                move_number += 1
+
+if __name__ == '__main__':
+    learn = QLearning()
+    learn.q_learning(0.3)
+    for state_action_value in learn.state_action_values:
+        for value in state_action_value:
+            print(round(value,1),end = "  ")
+        print()
