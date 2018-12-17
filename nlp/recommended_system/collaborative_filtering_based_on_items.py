0465165c95c441404029c0182af6a76d63aa863a
QilongPan <3102377627@qq.com>
2018-12-17 18:50:36 +0800

    test

--- nlp/recommended_system/collaborative_filtering_based_on_items.py
+++ nlp/recommended_system/collaborative_filtering_based_on_items.py
@@ -2,4 +2,10 @@
 # @Author: Qilong Pan
 # @Date:   2018-12-15 14:10:51
 # @Last Modified by:   Qilong Pan
-# @Last Modified time: 2018-12-15 14:10:51
+# @Last Modified time: 2018-12-17 13:51:30
+
+'''
+
+'''
+
+
