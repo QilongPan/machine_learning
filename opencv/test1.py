30d22cce0d1f90f1aa67b503bbb39591285f2e07
QilongPan <3102377627@qq.com>
2019-07-30 21:56:06 +0800

    test

--- opencv/test1.py
+++ opencv/test1.py
@@ -3,7 +3,7 @@
 # @Author  : QilongPan 
 # @Email   : 3102377627@qq.com
 import cv2
-img = cv2.imread("../image/elu.jpg")
+img = cv2.imread("C:/Users/Administrator/Desktop/test.jpg")
 cv2.namedWindow("image") #创建窗口并显示的是图像类型
 cv2.imshow("image",img)
 cv2.waitKey(0)        #等待事件触发，参数0表示永久等待
